f10ff4b2fc93c7822cee8a6672ecc071f6a9293a
Bruno Kemel <br.kemel@gmail.com>
2026-04-08 09:32:10 -0300

    pastas reoganizando

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import Header from '@/components/Header'
-import HeroBanner from '@/components/HeroBanner'
-import SponsorsSlider from '@/components/SponsorsSlider'
-import RaceCards from '@/components/RaceCards'
-import LocationSection from '@/components/LocationSection'
-import ContactSection from '@/components/ContactSection'
-import Footer from '@/components/Footer'
+import Header from '@/components/Header/Header'
+import HeroBanner from '@/components/HeroBanner/HeroBanner'
+import SponsorsSlider from '@/components/SponsorsSlider/SponsorsSlider'
+import RaceCards from '@/components/RaceCards/RaceCards'
+import LocationSection from '@/components/LocationMap/LocationSection'
+import ContactSection from '@/components/Contact/ContactSection'
+import Footer from '@/components/Footer/Footer'
 
 export default function Home() {
   return (
